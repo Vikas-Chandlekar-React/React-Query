eda06fc1d52a57d776191ca849479fa9b639762c
WorkAccVikas <workaccvikas@gmail.com>
2023-08-14 00:17:25 +0530

    one : c3

--- one/src/App.jsx
+++ one/src/App.jsx
@@ -1,21 +1,26 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import Home from "./components/Home.page";
 import SuperHeroes from "./components/SuperHeroes.page";
 import RQSuperHeroes from "./components/RQSuperHeroes.page";
 import Navbar from "./components/Navbar/Navbar";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/super-heroes" element={<SuperHeroes />} />
-          <Route path="/rq-super-heroes" element={<RQSuperHeroes />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/super-heroes" element={<SuperHeroes />} />
+            <Route path="/rq-super-heroes" element={<RQSuperHeroes />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </>
   );
 }
--- one/src/components/RQSuperHeroes.page.jsx
+++ one/src/components/RQSuperHeroes.page.jsx
@@ -1,7 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+  return axios.get(`http://localhost:4000/superheroes`);
+};
+
 function RQSuperHeroes() {
   console.count("RQSuperHeroes");
 
-  return <div>RQSuperHeroes Page</div>;
+  const { data, isLoading } = useQuery({
+    queryKey: ["super-heroes"],
+    queryFn: fetchSuperHeroes,
+  });
+
+  if (isLoading) {
+    return <h2>Loading.....</h2>;
+  }
+  return (
+    <div>
+      <h2>RQSuperHeroes Page</h2>
+      {data?.data?.map((hero) => (
+        <div key={hero.name}>{hero.name}</div>
+      ))}
+    </div>
+  );
 }
 
 export default RQSuperHeroes;
