cc686857d11b302ac2666280507a590a75ec3ead
WorkAccVikas <workaccvikas@gmail.com>
2023-08-14 19:07:51 +0530

    one : c13

--- one/src/components/RQSuperHeroes.page.jsx
+++ one/src/components/RQSuperHeroes.page.jsx
@@ -6,20 +6,29 @@ const fetchSuperHeroes = async () => {
   return await axios.get(`http://localhost:4000/superheroes`);
 };
 
+const onSuccess = (data) => {
+  console.log("Perform side effect after data fetching = ", data);
+};
+
+const onError = (error) => {
+  console.log("Perform side effect after encountering error = ", error);
+};
+
 function RQSuperHeroes() {
   console.count("RQSuperHeroes");
 
-  const { data, isLoading, isError, error, isFetching, refetch } = useQuery({
+  const { data, isLoading, isError, error, isFetching } = useQuery({
     queryKey: ["super-heroes"],
     queryFn: fetchSuperHeroes,
-    enabled: false,
+    onSuccess: onSuccess,
+    onError: onError,
   });
 
   console.log({ isLoading, isFetching });
 
-  // if (isLoading) {
-  //   return <h2>Loading.....</h2>;
-  // }
+  if (isLoading) {
+    return <h2>Loading.....</h2>;
+  }
 
   if (isError) {
     return <h2>{error.message}</h2>;
@@ -27,7 +36,6 @@ function RQSuperHeroes() {
   return (
     <div>
       <h2>RQSuperHeroes Page</h2>
-      <button onClick={refetch}>Fetch Data</button>
       {data?.data?.map((hero) => (
         <div key={hero.name}>{hero.name}</div>
       ))}
