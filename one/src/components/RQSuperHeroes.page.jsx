0e3e3108fde7254d24753836c8d92a8c5337d225
WorkAccVikas <workaccvikas@gmail.com>
2023-08-14 17:15:14 +0530

    one : c12

--- one/src/components/RQSuperHeroes.page.jsx
+++ one/src/components/RQSuperHeroes.page.jsx
@@ -1,32 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const fetchSuperHeroes = () => {
+const fetchSuperHeroes = async () => {
   console.count("After api call");
-  return axios.get(`http://localhost:4000/superheroes`);
+  return await axios.get(`http://localhost:4000/superheroes`);
 };
 
 function RQSuperHeroes() {
   console.count("RQSuperHeroes");
 
-  /**  DESC : refetchOnWindowFocus : false/time in ms
-   * Default : false
-   * If you want to api call after regular interval
-   * Note : it will not polling when component lost focus (not working on background)
-   * If you want to polling work on background then you must use refetchIntervalInBackground: true
-   */
-  const { data, isLoading, isError, error, isFetching } = useQuery({
+  const { data, isLoading, isError, error, isFetching, refetch } = useQuery({
     queryKey: ["super-heroes"],
     queryFn: fetchSuperHeroes,
-    refetchInterval: 2000,
-    refetchIntervalInBackground: true,
+    enabled: false,
   });
 
   console.log({ isLoading, isFetching });
 
-  if (isLoading) {
-    return <h2>Loading.....</h2>;
-  }
+  // if (isLoading) {
+  //   return <h2>Loading.....</h2>;
+  // }
 
   if (isError) {
     return <h2>{error.message}</h2>;
@@ -34,6 +27,7 @@ function RQSuperHeroes() {
   return (
     <div>
       <h2>RQSuperHeroes Page</h2>
+      <button onClick={refetch}>Fetch Data</button>
       {data?.data?.map((hero) => (
         <div key={hero.name}>{hero.name}</div>
       ))}
