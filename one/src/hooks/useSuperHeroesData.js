5389aa496498eec533201b4a23d6c3a6a9fa3f8b
WorkAccVikas <workaccvikas@gmail.com>
2023-08-16 01:14:34 +0530

    one : c26

--- one/src/hooks/useSuperHeroesData.js
+++ one/src/hooks/useSuperHeroesData.js
@@ -20,21 +20,67 @@ const addSuperHero = (hero) => {
 };
 
 // NOTE : For post,update,delete data use must use useMutation
+// export const useAddSuperHeroData = () => {
+//   const queryClient = useQueryClient();
+//   return useMutation(addSuperHero, {
+//     onSuccess: (data) => {
+//       // ACTION : Before, when we add data we manually fetch all data to show latest data to solve problem see below code
+//       // NOTE : It is useful when we add data in db and then refetch all data using invalidateQueries
+//       // queryClient.invalidateQueries(["super-heroes"]);
+//       // DESC : 2 API request 1 for post and 1 for get all data so we save additional api request for get all data
+//       // NOTE : setQueriesData : use for update then query cache data
+//       queryClient.setQueriesData(["super-heroes"], (oldQueryData) => {
+//         return {
+//           ...oldQueryData,
+//           data: [...oldQueryData.data, data.data],
+//         };
+//       });
+//     },
+//   });
+// };
+
+// TOPIC : OPTIMISTIC UPDATE
+/** DESC :  Updating the state before performing mutation under the assumption
+ *  that nothing can go wrong. It is typically give an impression that your app is
+ *  pleasing fast
+ */
 export const useAddSuperHeroData = () => {
   const queryClient = useQueryClient();
+
   return useMutation(addSuperHero, {
-    onSuccess: (data) => {
-      // ACTION : Before, when we add data we manually fetch all data to show latest data to solve problem see below code
-      // NOTE : It is useful when we add data in db and then refetch all data using invalidateQueries
-      // queryClient.invalidateQueries(["super-heroes"]);
-      // DESC : 2 API request 1 for post and 1 for get all data so we save additional api request for get all data
-      // NOTE : setQueriesData : use for update then query cache data
-      queryClient.setQueriesData(["super-heroes"], (oldQueryData) => {
+    /** DESC : onMutate :
+     *  It is called before mutation function is fired and is passed the same variables
+     *  the mutation function would receive
+     */
+    onMutate: async (newHero) => {
+      await queryClient.cancelQueries(["super-heroes"]);
+      const previousHeroData = queryClient.getQueryData(["super-heroes"]);
+
+      queryClient.setQueryData(["super-heroes"], (oldQueryData) => {
         return {
           ...oldQueryData,
-          data: [...oldQueryData.data, data.data],
+          data: [
+            ...oldQueryData.data,
+            { id: oldQueryData?.data?.length + 1, ...newHero },
+          ],
         };
       });
+      // DESC : It is used to rollback data in case the mutation errors out
+      return { previousHeroData };
+    },
+
+    /** DESC : onError :
+     *  It is called when mutation encounters error.
+     *  It contain 3 arguments.
+     */
+    onError: (_err, _newTodo, context) => {
+      queryClient.setQueryData(["super-heroes"], context.previousHeroData);
+    },
+    /** DESC : onSettled :
+     *  It is called when either successful or when it encounters an errors
+     */
+    onSettled: () => {
+      queryClient.invalidateQueries(["super-heroes"]);
     },
   });
 };
