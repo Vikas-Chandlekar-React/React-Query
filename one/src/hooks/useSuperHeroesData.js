cb221114f7be8b2486292df213d162076b3fbc9f
WorkAccVikas <workaccvikas@gmail.com>
2023-08-15 21:45:04 +0530

    one : c25

--- one/src/hooks/useSuperHeroesData.js
+++ one/src/hooks/useSuperHeroesData.js
@@ -23,10 +23,18 @@ const addSuperHero = (hero) => {
 export const useAddSuperHeroData = () => {
   const queryClient = useQueryClient();
   return useMutation(addSuperHero, {
-    onSuccess: () => {
+    onSuccess: (data) => {
       // ACTION : Before, when we add data we manually fetch all data to show latest data to solve problem see below code
       // NOTE : It is useful when we add data in db and then refetch all data using invalidateQueries
-      queryClient.invalidateQueries(["super-heroes"]);
+      // queryClient.invalidateQueries(["super-heroes"]);
+      // DESC : 2 API request 1 for post and 1 for get all data so we save additional api request for get all data
+      // NOTE : setQueriesData : use for update then query cache data
+      queryClient.setQueriesData(["super-heroes"], (oldQueryData) => {
+        return {
+          ...oldQueryData,
+          data: [...oldQueryData.data, data.data],
+        };
+      });
     },
   });
 };
